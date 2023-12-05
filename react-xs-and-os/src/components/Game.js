6570eddcb347b4ce699cdba0a8e7e7eb01d81b06
Wyatt-Drew <98035850+Wyatt-Drew@users.noreply.github.com>
2023-12-05 16:04:29 -0600

    MVP
    Added color
    Fixed all visual issues

--- react-xs-and-os/src/components/Game.js
+++ react-xs-and-os/src/components/Game.js
@@ -3,26 +3,37 @@ import React, { useState } from 'react';
 
 function Game() {
   const [board, setBoard] = useState(Array(9).fill(null));
+  const [winningSquares, setWinningSquares] = useState([]);
   const [xIsNext, setXIsNext] = useState(true);
 
   const handleClick = (index) => {
     const newBoard = [...board];
     if (calculateWinner(newBoard) || newBoard[index]) {
-      return; //do nothing
+      return; // do nothing if there's a winner
     }
     newBoard[index] = xIsNext ? 'X' : 'O';
+
+    const winner = calculateWinner(newBoard);
+    if (winner) {
+      setWinningSquares(calculateWinningSquares(newBoard, winner));
+    }
+
     setBoard(newBoard);
     setXIsNext(!xIsNext);
   };
 
-  const renderSquare = (index) => (
-    <button
-      className={`square ${board[index] ? 'clicked' : ''}`}
-      onClick={() => handleClick(index)}
-    >
-      {board[index]}
-    </button>
-  );
+  const renderSquare = (index) => {
+    const isWinningSquare = winningSquares.includes(index);
+
+    return (
+      <button
+        className={`square ${board[index] ? 'clicked' : ''} ${isWinningSquare ? 'winning' : ''}`}
+        onClick={() => handleClick(index)}
+      >
+        {board[index]}
+      </button>
+    );
+  };
 
   const winner = calculateWinner(board);
   const status = winner
@@ -31,7 +42,6 @@ function Game() {
 
   return (
     <div className="game">
-      
       <div className="board">
         <div className="board-row">
           {renderSquare(0)}
@@ -54,20 +64,38 @@ function Game() {
   );
 }
 
-// Helper function to calculate the winner
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
     [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-    [0, 4, 8], [2, 4, 6]              // Diagonals
+    [0, 4, 8], [2, 4, 6]             // Diagonals
   ];
+
   for (const line of lines) {
     const [a, b, c] = line;
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a]; // Return the winning symbol (X or O)
     }
   }
+
   return null; // no winner
+}
+
+function calculateWinningSquares(squares, winner) {
+  const lines = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+    [0, 4, 8], [2, 4, 6]              // Diagonals
+  ];
+
+  for (const line of lines) {
+    const [a, b, c] = line;
+    if (squares[a] === winner && squares[b] === winner && squares[c] === winner) {
+      return line.slice();
+    }
+  }
+
+  return [];
 }
 
 export default Game;
--- react-xs-and-os/src/index.js
+++ react-xs-and-os/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import App from './App.js';
-import './App.css';
 import './index.css';
 
 
